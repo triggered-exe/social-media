cb597bfced40abea6f5406d5b6920c48db766339
triggered-exe <harishankersharma648@gmail.com>
2024-01-14 20:50:56 +0530

    added frontend url of vercel

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:3000', // Allow requests from this origin
+    origin: ['http://localhost:3000','https://social-media-clone-mern-stack.vercel.app', process.env.Frontend_URL], // Allow requests from this origin
     credentials: true, // Include credentials in the request
   })
 );
