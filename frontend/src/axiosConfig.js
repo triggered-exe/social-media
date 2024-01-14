8344de5fbbd131153c60384ba7b983726d453255
triggered-exe <harishankersharma648@gmail.com>
2024-01-14 12:42:23 +0530

    backend for heroku

--- frontend/src/axiosConfig.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-// Create an instance of Axios with default configuration
-const instance = axios.create({
-  baseURL: 'http://127.0.0.1:8000', // Your API base URL
-  withCredentials: true, // Set withCredentials to true to send cookies
-});
-
-export default instance;
--- frontend/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import { reducer } from "./reduxSlice";
-export const store = configureStore({
-  reducer: {
-    reducer,
-  }
-})
