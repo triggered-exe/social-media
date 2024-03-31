b06aac067d38eb0a6cc941a28706a435ee4f3fcf
triggered-exe <harishankersharma648@gmail.com>
2024-03-31 17:15:48 +0530

    added backend url

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Create an instance of Axios with default configuration
 const instance = axios.create({
   // baseURL: 'http://127.0.0.1:8000', // Your API base URL
-  baseURL: 'https://twitter-backend-2029c7531810.herokuapp.com/', // Your API base URL
+  baseURL: 'https://social-media-clone-mern-stack.onrender.com/', // Your API base URL
   withCredentials: true, // Set withCredentials to true to send cookies
 });
 
